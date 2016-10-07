279973edc3afcffd09152cea077bef207c056583
etaiklein <etaiklein@gmail.com>
2016-10-07 13:12:44 -0400

    add results and add to google cal section, combined wiki/url input

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,76 +1,61 @@
+//toastr config options
 toastr.options = {
   "closeButton": true,
   "progressBar": true,
   "positionClass": "toast-top-full-width"
 }
 
-var validator = new FormValidator('tv_guide_form', [{
-  name: 'tv_guide_input',
-  display: 'wikipedia url',
-  rules: 'required|valid_url|callback_is_wikipedia'
-}], function(errors, event) {
-  
-  if (errors.length > 0) {
-    for (var error of errors) {
-      toastr.error(error.message);
+//set the base url for development or production
+// const baseURL = "http://localhost:3000/v1/" //development
+const baseURL = "https://wiki-scripts.herokuapp.com/v1/" //production
+
+//append the 10 most recently queried shows to the 'recent' list
+$.ajax({url: baseURL + "recent", 
+  success: (results) => {
+    for (let result of results) {
+      let myurl = `${baseURL}${escape(result)}/calendar.ics`;
+      $('#recent').append(`<div><a href=${myurl}>${result}.ics</a></div>`); 
+    }
+  },
+  error: (error) => {
+    if (error.status == 200){
+      toastr.success(error.responseText);
     }
   }
-
-  if (event.type == "submit"){
-    tvGuideSubmit(event);
-  }
-
 });
 
-validator.registerCallback('is_wikipedia', function(value) {
-  if (value.indexOf("wikipedia.org") > -1) {
-    return true;
-  }
-  return false;
-})
-.setMessage('is_wikipedia', "Please input a valid wikipedia url like 'https://en.wikipedia.org/wiki/List_of_Adventure_Time_episodes'");
 
-var tvGuideSubmit = function(event){
-	event.preventDefault();
-	var myurl = "https://wiki-scripts.herokuapp.com/v1/wiki?url=" + $("#tv_guide_input").val();
-  // var myurl = "http://localhost:3000/v1/wiki/?url=" + $("#tv_guide_input").val();
-	$.ajax({url: myurl, 
-    success: function(result){
-      download(result);
+$("#tv_guide_submit").click((event) => {
+  event.preventDefault();
+
+  let input = $("#tv_guide_input").val();
+  let myurl;
+
+  //hit the wiki endpoint if it's a wiki page -- else the name endpoint
+  if (input.indexOf("wikipedia.org") > -1) {
+    myurl = baseURL + "wiki/?url=" + input;
+  } else {
+    myurl = baseURL + "" + escape(input) + "/calendar.ics" ;
+  }
+
+  //hit the endpoint to findOrCreate the calendar
+  $.ajax({url: myurl, 
+    success: (result) => {
+      //append the link below search
+      linkify(myurl, input);
     },
-    error: function(error){
+    error: (error) => {
       if (error.status == 200){
         toastr.success(error.responseText);
+      } else {
+        toastr.error("Unable to process page. The error has been logged");
       }
     }
   });
-}
+});
 
-function getParameterByName(name, url = window.location.href) {
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-
-var tvGuideQuery = function(){
-  var myurl = "https://wiki-scripts.herokuapp.com/v1/wiki/calendar?title=" + getParameterByName("title");
-  // var myurl = "http://localhost:3000/v1/wiki/calendar?title=" + getParameterByName("title");
-  $.ajax({url: myurl, 
-    success: function(result){
-      download(result);
-    },
-    error: function(error){
-      if (error.status == 200){
-      }
-    }
-  });
-}
-
-var download = function(data) {
-  var uriContent = "data:text/calendar," + encodeURIComponent(data);
-  location.href = uriContent;
+//reveal the results section and append the new url to it
+const linkify = (url, input) => {
+  $("#results").removeClass("hidden");
+  $('#results').append("<div><a href="+url+">"+ input +".ics</a><div>");
 }
